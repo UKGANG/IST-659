52ddf6312d59116a3e86a580ab9be763ba1fa807
JIAN JIAN <ukgang@hotmail.com>
2019-11-26 15:50:21 -0500

    Repository cleanup

--- bin/main/static/js/model/rental.js
+++
@@ -1,8 +0,0 @@
-define([ 'backbone'], function(Backbone) {
-    var Rental = Backbone.Model.extend({
-        url: "/fge/reservation/gear",
-        idAttribute: "rid",
-    });
-
-    return Rental;
-});
--- bin/main/static/js/model/reservation.js
+++
@@ -1,9 +0,0 @@
-define([ 'backbone'], function(Backbone) {
-    var Reservation = Backbone.Model.extend({
-    	url : "/fge/court/reservation",
-        idAttribute: "rid",
-
-    });
-
-    return Reservation;
-});
